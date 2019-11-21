4c357e22bfa3c3e25b2c880e5cb08c44b6346803
Yunus Emre <yunusemre.kirkanahtar@cutesafe.com>
2019-11-21 15:40:31 +0300

    Update swagger to latest version

--- Extensions/SwaggerServiceExtension.cs
+++ Extensions/SwaggerServiceExtension.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerUI;
 
@@ -7,35 +9,45 @@ namespace CustomFramework.WebApiUtils.Extensions
 {
     public static class SwaggerServiceExtension
     {
-        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services, string apiName, int version)
         {
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1.0", new Info { Title = "Main API v1.0", Version = "v1.0" });
+                c.SwaggerDoc($"v{version}", new OpenApiInfo { Title = apiName, Version = $"v{version}" });
 
-                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
-                {
-                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-                    Name = "Authorization",
-                    In = "header",
-                    Type = "apiKey"
-                });
+                c.AddSecurityDefinition("Bearer", //Name the security scheme
+                    new OpenApiSecurityScheme
+                    {
+                        Description = "JWT Authorization header using the Bearer scheme.",
+                        Type = SecuritySchemeType.Http, //We set the scheme type to http since we're using bearer authentication
+                        Scheme = "bearer" //The name of the HTTP Authorization scheme to be used in the Authorization header. In this case "bearer".
+                    });
 
-                c.DocumentFilter<SwaggerSecurityRequirementsDocumentFilter>();
+                c.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement{
+                    {
+                        new OpenApiSecurityScheme{
+                            Reference = new OpenApiReference{
+                                Id = "Bearer", //The name of the previously defined security scheme.
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        },new List<string>()
+                    }});
             });
 
             return services;
         }
 
-        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app, string appName, int version)
         {
             app.UseSwagger();
+
             app.UseSwaggerUI(c =>
             {
-                const string url = "/swagger/v1.0/swagger.json";
-                c.SwaggerEndpoint(url, "Versioned API v1.0");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{appName} V{version}");
                 c.DocExpansion(DocExpansion.None);
-            });
+                c.RoutePrefix = string.Empty;
+            });            
 
             return app;
         }
